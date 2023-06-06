2d5887421f2e7bb0287d29a38771623610e74a44
Fellyipe <felipebueno2004@hotmail.com>
2023-06-06 14:46:05 -0300

    ...

--- Model/Venda.cs
+++ Model/Venda.cs
@@ -5,13 +5,14 @@ namespace Game1{
     {
         public int Id { get; set; }
         public Cliente Cliente { get; set; }
-        public List<Produto> Produtos { get; set; }
+        public List<VendaProduto>? Produtos { get; set; }
+        public DateTime Data { get; set; }
 
-        public Venda(int id, Cliente cliente)
+        public Venda(int id, Cliente cliente, DateTime data)
         {
             Id = id;
             Cliente = cliente;
-            Produtos = new List<Produto>();
+            Data = data;
         }
     }
 
---
+++ Model/VendaProduto.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1
+
+{
+    public class VendaProduto
+    {
+        
+        public long Id { get; set; }
+        public Produto? Produto { get; set; }
+        public double Quantidade { get; set; }
+        public double PrecoUnitario { get; set; }
+
+      public VendaProduto(int id, Produto produto, int quantidade, double precoUnitario)
+        {
+            Id = id;
+            Produto = produto;
+            Quantidade = quantidade;
+            PrecoUnitario = precoUnitario;
+        }
+    }
+}
